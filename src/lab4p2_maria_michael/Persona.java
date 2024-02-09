3f950fb6dba04c12eb7bdc44e226a97ef9a281b7
MariaJ-IreneS <misinclair15@unitec.edu>
2024-02-09 15:20:45 -0600

    1Clase

---
+++ src/lab4p2_maria_michael/Persona.java
@@ -0,0 +1,21 @@
+package lab4p2_maria_michael;
+
+public abstract class Persona {
+
+    protected String nombre;
+    protected int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", edad=" + edad + '}';
+    }
+
+}
