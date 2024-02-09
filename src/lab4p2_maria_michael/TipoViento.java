9ce024ca89d9353df95a21bf01cf1a58c0e3f33d
MariaJ-IreneS <misinclair15@unitec.edu>
2024-02-09 16:13:58 -0600

    4tipo

--- src/lab4p2_maria_michael/TipoViento.java
+++ src/lab4p2_maria_michael/TipoViento.java
@@ -9,7 +9,29 @@ public class TipoViento extends Agentes {
     }
 
     public void Dano(Agentes agente) {
+        Random aleatorio = new Random();
+        int numAleatorio = aleatorio.nextInt(8);
 
+        if (numAleatorio <= 6) {
+            double dano = 0.0;
+
+            if (agente instanceof TipoFuego) {
+                dano = getPuntosAtaque() * 1.5;
+                double nuevodanio;
+                nuevodanio = getVida() - dano;
+                setVida(nuevodanio);
+            } else if (agente instanceof TipoAgua) {
+                dano = getPuntosAtaque() * 0.7;
+                double nuevodanio;
+                nuevodanio = getVida() - dano;
+                setVida(nuevodanio);
+            } else if (agente instanceof TipoViento) {
+                dano = getPuntosAtaque() * 1.1;
+                double nuevodanio;
+                nuevodanio = getVida() - dano;
+                setVida(nuevodanio);
+            }
+        }
     }
 
 }
