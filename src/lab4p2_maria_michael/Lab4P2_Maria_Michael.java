5dd097269c1d4be5e22460369eb2bc1fd0d34ab0
MariaJ-IreneS <misinclair15@unitec.edu>
2024-02-09 17:29:27 -0600

    Case1

--- src/lab4p2_maria_michael/Lab4P2_Maria_Michael.java
+++ src/lab4p2_maria_michael/Lab4P2_Maria_Michael.java
@@ -28,6 +28,19 @@ public class Lab4P2_Maria_Michael {
         TipoViento agenteViento2 = new TipoViento("AgenteViento2", 25, 105.0, 120.0, 20);
         TipoViento agenteViento3 = new TipoViento("AgenteViento3", 20, 95.0, 110.0, 22);
 
+        ArrayList<Agentes> listaAgentes = new ArrayList();
+        listaAgentes.add(agenteFuego1);
+        listaAgentes.add(agenteFuego2);
+        listaAgentes.add(agenteFuego3);
+        listaAgentes.add(agenteAgua1);
+        listaAgentes.add(agenteAgua2);
+        listaAgentes.add(agenteAgua3);
+        listaAgentes.add(agenteTierra1);
+        listaAgentes.add(agenteTierra2);
+        listaAgentes.add(agenteTierra3);
+        listaAgentes.add(agenteViento1);
+        listaAgentes.add(agenteViento2);
+        listaAgentes.add(agenteViento3);
         // Crear usuarios con agentes precreados
         Usuario persona1 = new Usuario("julio@gmail.com", "julio05", "julio2005", 5000.0, new ArrayList<>(), "Julio", 25);
         persona1.agente.add(agenteFuego1);
@@ -70,7 +83,7 @@ public class Lab4P2_Maria_Michael {
                 usuario = persona3;
                 inicioSesionExitoso = true;
             } else {
-                System.out.println("Inicio de sesion fallido. Por favor, intente nuevamente.");
+                System.out.println("Inicio de sesión fallido. Por favor, intente nuevamente.");
             }
 
         } while (!inicioSesionExitoso);
@@ -83,13 +96,82 @@ public class Lab4P2_Maria_Michael {
             System.out.println("2. Ver agentes");
             System.out.println("3. Salir");
             switch (opcionMenu) {
-
                 case 1:
+                    boolean jugar = true;
+                    while (jugar) {
+                        int otroUsuario = random.nextInt(usuario.agente.size() - 1);
+                        while (otroUsuario == listaU.indexOf(usuario)) {
+                            otroUsuario = random.nextInt(usuario.agente.size() - 1);
+                        }
+                        //imprimir agentes disponibles del usuario
+                        for (int i = 0; i < usuario.agente.size(); i++) {
+                            System.out.println(i + " )" + usuario.agente.get(i));
+                        }
+                        //seleccionar agente
+                        System.out.print("Ingrese la posicion del agente a usar: ");
+                        int posicionAgente = entrada.nextInt();
+                        //Imprimir agentes del rival
+                        for (int i = 0; i < listaU.get(otroUsuario).agente.size(); i++) {
+                            System.out.print(i + " )" + listaU.get(i));
+                        }
+                        System.out.print("Ingrese el usuario que quiere atacar: ");
+                        int agenteRival = entrada.nextInt();
+
+                        //ataque del usuario
+                        while (listaU.get(otroUsuario).agente.get(agenteRival).vida <= 0) {
+
+                            System.out.println("Agente sin vida, seleccione otro agente: ");
+                            agenteRival = entrada.nextInt();
+
+                        }
+                        usuario.agente.get(posicionAgente).Dano(listaU.get(otroUsuario).agente.get(agenteRival));
+
+                        //ataque del rival
+                        int randomAtacar = random.nextInt(usuario.agente.size());
+                        int randomElegir = random.nextInt(listaU.get(otroUsuario).agente.size());
+                        while (usuario.agente.get(randomAtacar).vida <= 0) {
+                            randomAtacar = random.nextInt(usuario.agente.size());
+                        }
+                        listaU.get(otroUsuario).agente.get(randomElegir).Dano(usuario.agente.get(randomAtacar));
+                        int contador1 = 0;
+                        int contador2 = 0;
+                        for (int i = 0; i < usuario.agente.size(); i++) {
+                            if (usuario.agente.get(i).vida <= 0) {
+                                contador1++;
+                            }
+                        }
+                        for (int i = 0; i < listaU.get(otroUsuario).agente.size(); i++) {
+                            if (listaU.get(otroUsuario).agente.get(i).vida <= 0) {
+                                contador2++;
+                            }
+                        }
+
+                        if (contador1 == 3) {
+                            System.out.println("GANÓ EL JUGAR OTRO JUGADOR LLAMADO: " + listaU.get(otroUsuario).nombre);
+                            double dineroNuevo = listaU.get(otroUsuario).getDinero() + 3000;
+                            listaU.get(otroUsuario).setDinero(dineroNuevo);
+                            jugar = false;
+                        } else if (contador2 == 3) {
+                            System.out.println("GANASTE VOS" + usuario.nombre);
+                            double dineroNuevo = usuario.getDinero() + 3000;
+                            usuario.setDinero(dineroNuevo);
+                            jugar = false;
+                        }
+                    }
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    bandera = false;
+
                     break;
 
-                case 2:
-                    break;
+                default:
+                    System.out.println("Opcion Incorrecta");
             }
+
         }
     }
 }
