6bd3593175831fb234f090366816f3cf4dd678d6
MariaJ-IreneS <misinclair15@unitec.edu>
2024-02-09 16:09:16 -0600

    3tipo

--- src/lab4p2_maria_michael/TipoTierra.java
+++ src/lab4p2_maria_michael/TipoTierra.java
@@ -1,9 +1,36 @@
 package lab4p2_maria_michael;
 
+import java.util.Random;
+
 public class TipoTierra extends Agentes {
 
     public TipoTierra(double precio, double vida, int puntosAtaque, String nombre, int edad) {
         super(precio, vida, puntosAtaque, nombre, edad);
     }
 
+    public void Dano(Agentes agente) {
+        Random aleatorio = new Random();
+        int numAleatorio = aleatorio.nextInt(8);
+
+        if (numAleatorio <= 6) {
+            double dano = 0.0;
+
+            if (agente instanceof TipoFuego) {
+                dano = getPuntosAtaque() * 0.7;
+                double nuevodanio;
+                nuevodanio = getVida() - dano;
+                setVida(nuevodanio);
+            } else if (agente instanceof TipoAgua) {
+                dano = getPuntosAtaque() * 1.3;
+                double nuevodanio;
+                nuevodanio = getVida() - dano;
+                setVida(nuevodanio);
+            } else if (agente instanceof TipoViento) {
+                dano = getPuntosAtaque() * 0.4;
+                double nuevodanio;
+                nuevodanio = getVida() - dano;
+                setVida(nuevodanio);
+            }
+        }
+    }
 }
--- src/lab4p2_maria_michael/TipoViento.java
+++ src/lab4p2_maria_michael/TipoViento.java
@@ -1,9 +1,15 @@
 package lab4p2_maria_michael;
 
+import java.util.Random;
+
 public class TipoViento extends Agentes {
 
     public TipoViento(double precio, double vida, int puntosAtaque, String nombre, int edad) {
         super(precio, vida, puntosAtaque, nombre, edad);
+    }
+
+    public void Dano(Agentes agente) {
+
     }
 
 }
